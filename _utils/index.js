75aa314b99e5faad5acd41e691f6d03e18769434
Marcos Mellado <marcos@airtame.com>
2017-07-18 12:58:42 +0200

    Added shallowCompareOptionsArray util function

--- _utils/index.js
+++ _utils/index.js
@@ -3,13 +3,35 @@
  * functions needed by components in this library to work properly.
  */
 
+ /**
+  * Makes a shallow comparisson between 2 arrays containing plain objects. Returns true if the
+  * shallow comparisson between the objects in the array is true. This also evaluates that they are
+  * in the same order in the array
+  *
+  * @param {any} arr1 - The options array to compare
+  * @param {any} arr2 - The options array to compare with
+  * @return {Boolean} - Whether the array contents and order are the same or not
+  */
+export function shallowCompareOptionsArray(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    return arr1.every((el, i) => shallowObjectCompare(arr1[i], arr2[i]));
+  } else {
+    return false;
+  }
+}
+
 /**
  * Makes a shallow comparison between 2 objects
+ *
  * @param {Object} obj1 - The object to compare
  * @param {Object} obj2 - The object to compare with
- * @return {Boolean} - Whether the object contents and the order of its keys are the same or not
+ * @return {Boolean} - Whether the object contents are same or not
  */
-export default function shallowObjectCompare(obj1, obj2) {
+export function shallowObjectCompare(obj1, obj2) {
+  if (!isPlainObject(obj1) || !isPlainObject(obj2)) {
+    return false;
+  }
+
   const obj1Keys = Object.keys(obj1);
   if (obj1Keys.length === Object.keys(obj2).length) {
     return obj1Keys.every(key => obj1[key] === obj2[key]);
@@ -18,3 +40,12 @@ export default function shallowObjectCompare(obj1, obj2) {
   }
 }
 
+/**
+ * Verifies if a value is a plain object
+ *
+ * @param {any} obj - The value to validate
+ * @returns {Boolean} - Whether the value is a plain object or not
+ */
+export function isPlainObject(obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
+}
--- _utils/utils.spec.js
+++ _utils/utils.spec.js
@@ -1,4 +1,8 @@
-import shallowObjectCompare from './index.js';
+import {
+  shallowObjectCompare,
+  shallowCompareOptionsArray,
+  isPlainObject,
+} from './index.js';
 
 describe('_utils/shallowObjectCompare', () => {
   it('should return false when objects have different length', () => {
@@ -22,10 +26,87 @@ describe('_utils/shallowObjectCompare', () => {
     expect(shallowObjectCompare(obj1, obj2)).to.eql(true);
   });
 
-  it('should return fatruenlse when objects have same content in the same order', () => {
+  it('should return true when objects have same content in the same order', () => {
     const obj1 = { a: 1, b: 2 };
     const obj2 = { a: 1, b: 2 };
 
     expect(shallowObjectCompare(obj1, obj2)).to.eql(true);
+  });
+
+
+  it('should return false if any of the parameters is not an object', () => {
+    const obj1 = { a: 1, b: 2 };
+    const obj3 = 'foo';
+    const obj4 = [1, 2, 3];
+
+    expect(shallowObjectCompare(obj1, obj3)).to.eql(false);
+    expect(shallowObjectCompare(obj3, obj1)).to.eql(false);
+    expect(shallowObjectCompare(obj4, obj1)).to.eql(false);
+    expect(shallowObjectCompare(obj1, obj4)).to.eql(false);
+    expect(shallowObjectCompare(obj3, obj4)).to.eql(false);
+  });
+});
+
+describe('_utils/shallowCompareOptionsArray', () => {
+  it('should return false when arrays have different length', () => {
+    const arr1 = [{ a: 1, b: 2 }, { a: 1, b: 2 }];
+    const arr2 = [{ a: 1, b: 2 }];
+
+    expect(shallowCompareOptionsArray(arr1, arr2)).to.eql(false);
+  });
+
+  it('should return false when arrays have different content', () => {
+    const arr1 = [{ a: 1, b: 2 }, { a: 1, b: 2 }];
+    const arr2 = [{ a: 2, b: 3 }, { a: 2, b: 3 }];
+
+    expect(shallowCompareOptionsArray(arr1, arr2)).to.eql(false);
+  });
+
+  it('should return false when arrays have same content in different order', () => {
+    const arr1 = [{ a: 1, b: 2 }, { a: 3, b: 4 }];
+    const arr2 = [{ a: 3, b: 4 }, { a: 1, b: 2 }];
+
+    expect(shallowCompareOptionsArray(arr1, arr2)).to.eql(false);
+  });
+
+  it('should return true when objects have same content in the same order', () => {
+    const arr1 = [{ a: 1, b: 2 }, { a: 1, b: 2 }];
+    const arr2 = [{ a: 1, b: 2 }, { a: 1, b: 2 }];
+
+    expect(shallowCompareOptionsArray(arr1, arr2)).to.eql(true);
+  });
+});
+
+describe('_utils/isPlainObject', () => {
+  it('should return true for an object', () => {
+    expect(isPlainObject({ a: 1, b: 2 })).to.eql(true);
+  });
+
+  it('should return false for an array', () => {
+    expect(isPlainObject([1, 2, 3])).to.eql(false);
+  });
+
+  it('should return false for null', () => {
+    expect(isPlainObject(null)).to.eql(false);
+  });
+
+  it('should return false for a function', () => {
+    expect(isPlainObject(function () { return 5;})).to.eql(false);
+  });
+
+  it('should return false for a function returning an object', () => {
+    expect(isPlainObject(function () { return { a: 1, b: 2 } })).to.eql(false);
+  });
+
+  it('should return false for a string', () => {
+    expect(isPlainObject('foo')).to.eql(false);
+  });
+
+  it('should return false for a number', () => {
+    expect(isPlainObject(5)).to.eql(false);
+  });
+
+  it('should return false for undefined', () => {
+    expect(isPlainObject(undefined)).to.eql(false);
   });
 });
